3c71d4bd1f3e39e1c6f23dc25ada22c21e655a2b
dkohn <douglasjkohn@gmail.com>
2023-06-17 12:33:16 -0500

    Added Link To for the navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className="nav-links">
-      <a href="/">Home </a>
-      <a href="./About">About </a>
-      <a href="#">Old Stuff </a>
-      <a href="#">Contact</a>
+      <Link to="/">Home </Link>
+      <Link to="./About">About </Link>
+      <Link to="#">Old Stuff </Link>
+      <Link to="#">Contact</Link>
     </div>
   );
 };
